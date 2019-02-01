a0ffbafe9b153a3e3bf3c64a0f06a4d536a892d2
dramsammy <dramsammy@stuy.edu>
2019-01-31 20:25:03 -0500

    added some comments to wrap my head around the process

--- recursion.java
+++ recursion.java
@@ -14,14 +14,14 @@ public class recursion{
   //   return 0;
   // }
   public static void main(String[] args) {
-    System.out.println(fib(15));
+    System.out.println(fib(2));
   }
   private static int fibMemorize(int n, int zero, int one){
-    if (n == 0){
+    if (n == 0){ //base case starts off with 0 increases in value for tree... memorized value
       return zero;
     }
     if (n > 0){
-      return fibMemorize(n - 1, one, one + zero);
+      return fibMemorize(n - 1, one, one + zero); // decreases n value from start and just adds last two values... n = 2, zero = 0, one = 1, n = 1, zero = 1, one = 1, n = 0, zero = 1, one = 2
     }
     return 0;
   }
